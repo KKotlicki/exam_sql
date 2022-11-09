04765013983c39aac1e3191e29f30b5de60ec043
kotlik <72322890+KKotlicki@users.noreply.github.com>
2022-11-09 13:34:47 +0100

    10

--- ans.sql
+++ ans.sql
@@ -1,12 +1,10 @@
--- Zadanie 1
 CREATE EXTENSION postgis;
 SELECT postgis_full_version();
 
+-- Zadanie 1
 CREATE TYPE SL_KlasaCieku AS ENUM ('rzeka', 'kanal', 'struga');
 
 CREATE TYPE SL_KlasaZbiornika AS ENUM ('jezioro', 'staw', 'morze');
-
-CREATE TABLE Rzeka (
     rzeka_id serial PRIMARY KEY,
     nazwa varchar(255),
     kod_MPHP varchar(255)
@@ -19,7 +17,7 @@ CREATE TABLE SegmentCieku (
     dlugosc integer
 );
 
-ALTER TABLE ZbiornikWodny ADD COLUMN geom geometry(LINESTRING, 2180);
+ALTER TABLE SegmentCieku ADD COLUMN geom geometry(LINESTRING, 2180);
 ALTER TABLE SegmentCieku ADD CONSTRAINT SegmentCieku_geom_check CHECK (ST_IsValid(geom));
 
 CREATE TABLE Rzeka_SegmentCieku (
@@ -59,10 +57,9 @@ ALTER TABLE SegmentCieku ADD COLUMN zarzad_gospodarki_wodnej_id integer REFERENC
 SELECT COUNT(*) FROM SegmentCieku WHERE ST_DWithin(geom, (SELECT geom FROM ZbiornikWodny WHERE nazwa = 'Z1'), 100);
 
 -- Zadanie 3
--- Napisz w języku SQL zapytanie, które wyświetli nazwy zbiorników znajdujących się na terenie największego Zarządu Gospodarki Wodnej. Skorzystaj z funkcji przestrzennej.
+-- Napisz w języku SQL zapytanie, które wyświetli nazwy zbiorników znajdujących się na terenie największego Zarządu Gospodarki Wodnej. Skorzystaj z funkcji ST_Area.
 SELECT nazwa FROM ZbiornikWodny WHERE zarzad_gospodarki_wodnej_id = (SELECT zarzad_gospodarki_wodnej_id FROM ZarzadGospodarkiWodnej ORDER BY ST_Area(geom) DESC LIMIT 1);
 
 -- Zadanie 4
 -- Napisz w języku SQL zapytanie, które wyświetli listę o postaci: nazwa morza, liczba segmentów cieków wpływających do tego morza.
 SELECT nazwa, COUNT(*) FROM ZbiornikWodny JOIN ZbiornikWodny_SegmentCieku ON ZbiornikWodny.zbiornik_wodny_id = ZbiornikWodny_SegmentCieku.zbiornik_wodny_id JOIN SegmentCieku ON ZbiornikWodny_SegmentCieku.segment_cieku_id = SegmentCieku.segment_cieku_id WHERE klasa_zbiornika = 'morze' GROUP BY nazwa;
-
